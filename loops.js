7c5e1e3f3f3f9d73052b9ca9afc2404ce9017cd8
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-07 13:00:07 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -14,6 +14,18 @@ function whileLoop(n){
   }
   return "done";
 }
+
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("doo-bee-doo-bee-doo");
+} while (incrementVariable() < 5);
+
 function doWhileLoop(num){
   
 }
